5ac716cf0e2b8b29dff08acc5876fa3adc6b1f97
Mahmuda Begum <mahmuda.begum@v2solutions.com>
2025-03-27 19:17:46 +0530

    update readme

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-todo-march-2025/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
